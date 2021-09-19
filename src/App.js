27796e8dafcd90a742bb21239239f1459863e5a6
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2021-09-19 11:16:38 -0400

    Fix stuff

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { MdDashboard, MdInsertChart, MdVideocam, MdPhotoCamera, MdAccountCircle } from "react-icons/md";
 import Dashboard from "./pages/Dashboard.js";
-import Analytics from "./pages/Analytics.js";
 import Cameras from "./pages/Cameras.js";
 import Logo from "./Hive-HQ_Logo.png";
 
@@ -16,11 +15,15 @@ function App() {
           <img className="logo" alt="logo" src={Logo} />
           <ul className="links">
             <a href="/"><li className={activeClass("/")}><MdDashboard className="icon" />Dashboard</li></a>
-            <a href="/analytics" className="inactive"><li className={activeClass("/analytics")}><MdInsertChart className="icon" />Analytics</li></a>
-            <a href="/liveview" className="inactive"><li className={activeClass("/liveview")}><MdVideocam className="icon" />Live View</li></a>
+            <div className="inactive-wrapper">
+              <a href="/analytics" className="inactive"><li className={activeClass("/analytics")}><MdInsertChart className="icon" />Analytics</li></a>
+              <a href="/liveview" className="inactive"><li className={activeClass("/liveview")}><MdVideocam className="icon" />Live View</li></a>
+            </div>
             <a href="/cameras"><li className={activeClass("/cameras")}><MdPhotoCamera className="icon" />Cameras</li></a>
             <hr />
-            <a href="/profile" className="inactive"><li className={activeClass("/profile")}><MdAccountCircle className="icon" />Profile</li></a>
+            <div className="inactive-wrapper">
+              <a href="/profile" className="inactive"><li className={activeClass("/profile")}><MdAccountCircle className="icon" />Profile</li></a>
+            </div>
           </ul>
         </div>
         <div className="content">
